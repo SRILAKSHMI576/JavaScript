0bd59d4f86166d83c3238dbb67814d4d8d05592d
srilakshmi576 <srilakshmigulla576@gmail.com>
2019-05-19 18:52:54 +0530

    Checking Varable Type

---
+++ VarableType.js
@@ -0,0 +1,11 @@
+let number = 5;
+console.log("Type of 'number': " + typeof number);
+
+let number_1 = new Number(5);
+console.log("Type of 'number_1': " + typeof number_1);
+
+let number_2 = null;
+console.log("Type of 'number_2': " + typeof number_2);
+
+let number_3 = undefined;
+console.log("Type of 'number_3': " + typeof number_3);
