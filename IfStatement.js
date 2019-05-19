5d4a6fb71292cf5a9c9cae910320e87aa42c01c3
srilakshmi576 <srilakshmigulla576@gmail.com>
2019-05-19 22:23:02 +0530

    Comarision Between Two Numbers

---
+++ IfStatement.js
@@ -0,0 +1,11 @@
+let firstNumber = 4;
+let secondNumber = 2;
+var biggestNumber;
+if (firstNumber > secondNumber) {
+  biggestNumber = firstNumber;
+}
+if (firstNumber < secondNumber) {
+  biggestNumber = secondNumber;
+}
+
+console.log("the biggest number is: " + biggestNumber);
