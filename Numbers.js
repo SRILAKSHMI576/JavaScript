83e8f86267b9338ade98328d3d7f7ebb72c3b263
srilakshmi576 <srilakshmigulla576@gmail.com>
2019-05-19 15:54:40 +0530

    Numbers

---
+++ Numbers.js
@@ -0,0 +1,9 @@
+let count = 5;
+let pi = 3.14954;
+let integer = new Number(100);
+integer += 1;
+let biggestNum = Number.MAX_VALUE;
+console.log(count);
+console.log(pi);
+console.log(integer);
+console.log(biggestNum);
