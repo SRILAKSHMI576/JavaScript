e2011b2cc3f8f70d8674b7f0c969919d65d56533
srilakshmi576 <srilakshmigulla576@gmail.com>
2019-05-19 20:36:27 +0530

    Logical Opearators

---
+++ LogicalOperators.js
@@ -0,0 +1,7 @@
+let expression = true;
+let secondExpession = false;
+
+console.log(expression && secondExpession);
+console.log(expression || secondExpession);
+console.log(!secondExpession);
+console.log(5 > 7 && expression == secondExpession);
