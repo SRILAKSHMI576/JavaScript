49d3ae7d0a4fb5cd382e07b20845b6d2b3d31128
srilakshmi576 <srilakshmigulla576@gmail.com>
2019-05-19 17:57:52 +0530

    Parsing String To Numbers

---
+++ ParsingStringsToNumbers.js
@@ -0,0 +1,9 @@
+let numberString = "123";
+let floatString = "12.3";
+console.log(parseInt(numberString));
+console.log(parseFloat(floatString) + 0.7);
+
+let str_1 = "123Hello";
+let str_2 = "12.3Hello";
+console.log(parseInt(str_1));
+console.log(parseFloat(str_2));
