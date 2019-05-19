1724039a7b4142e101c85c99ad5d715667bf4531
srilakshmi576 <srilakshmigulla576@gmail.com>
2019-05-19 17:39:49 +0530

    print the String names

---
+++ String.js
@@ -0,0 +1,9 @@
+let welcome = "welcome to Nodejs ";
+let name = "john" + " " + "Doe";
+let greeting = welcome + "," + name + "!";
+console.log(greeting);
+
+let firstName = "sri";
+let lasName = "lakshmi";
+let fullName = "Your full name is: " + firstName + lasName;
+console.log(fullName);
