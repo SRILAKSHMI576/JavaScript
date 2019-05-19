776cee78dba9d6eefcbe112fc6467926689ee334
srilakshmi576 <srilakshmigulla576@gmail.com>
2019-05-19 22:03:13 +0530

    All Opearators

---
+++ Operators.js
@@ -0,0 +1,15 @@
+console.log("First" + "Second" + 1 + 5);
+
+console.log(Math.PI);
+
+console.log(2 / (0 + 2));
+
+var names = ["John", "george", "Jim"];
+console.log(names[0]);
+
+let a = 6;
+b = 4;
+console.log(a < b ? "yes" : "no");
+
+let c = 3.14159;
+console.log(typeof c);
