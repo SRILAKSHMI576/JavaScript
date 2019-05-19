42fded54bd6b3d1863d1e80af68f1a2630c16730
srilakshmi576 <srilakshmigulla576@gmail.com>
2019-05-19 16:07:24 +0530

    Numbres Conversions

---
+++ NumbersConversion.js
@@ -0,0 +1,9 @@
+let valueDouble = 8.75;
+let valueInt = valueDouble | 0;
+let roundInt = (valueDouble + 0.5) | 0;
+console.log(valueInt);
+console.log(roundInt);
+
+let str = "1234";
+let i = str | 0;
+console.log(i + 1);
