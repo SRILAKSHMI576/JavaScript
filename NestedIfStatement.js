830468414fa16a07e785438b9f69ae68bb683b46
srilakshmi576 <srilakshmigulla576@gmail.com>
2019-05-20 11:15:03 +0530

    Nested If Statememt Example

---
+++ NestedIfStatement.js
@@ -0,0 +1,14 @@
+var diceNumber = 4;
+if (diceNumber == 1) {
+  Console.log("The dice number is 1");
+} else if (diceNumber % 2 == 0) {
+  if (diceNumber == 2) {
+    console.log("The dice number is 2");
+  } else if (diceNumber == 4) {
+    console.log("The dice number is 4");
+  } else if (diceNumber == 6) {
+    console.log("The dice number is 6");
+  } else {
+    console.log("The number hsas to be betewen 1 and 2");
+  }
+}
