60bfdec0e13dde12d54e141aa913eaf9f2eec06d
srilakshmi576 <srilakshmigulla576@gmail.com>
2019-05-20 17:11:07 +0530

    Declaring ways of Arrays'

---
+++ DeclaringArrays.js
@@ -0,0 +1,13 @@
+var arr = new Array(1, 2, 3, 4, 5);
+var arrTwo = new Array(10);
+var mixedArr = [1, new Date(), "hello"];
+var matrix = [
+  ["0,0", "0,1", "0,2"],
+  ["1,0", "1,1", "1,2"],
+  ["2,0", "2,1", "2,2"]
+];
+
+console.log(arr);
+console.log(arrTwo);
+console.log(mixedArr);
+console.log(matrix);
