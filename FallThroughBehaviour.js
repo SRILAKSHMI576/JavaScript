8853366e237a4a11c4212d893e08dd63fd09d763
srilakshmi576 <srilakshmigulla576@gmail.com>
2019-05-20 11:41:17 +0530

    Fall Through Behaviour

---
+++ FallThroughBehaviour.js
@@ -0,0 +1,19 @@
+var day = 3;
+switch (day) {
+  case 1:
+    console.log("sunday");
+  case 2:
+    console.log("Monday");
+  case 3:
+    console.log("Tuesday");
+  case 4:
+    console.log("Wednesday");
+  case 5:
+    console.log("Thursday");
+  case 6:
+    console.log("Friday");
+  case 7:
+    console.log("Saturday");
+  case 8:
+    console.log("The number has to be between 1 and 7");
+}
