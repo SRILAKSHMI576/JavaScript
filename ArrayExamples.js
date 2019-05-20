c26f2cee11fec575a014cad18121f21937899af4
srilakshmi576 <srilakshmigulla576@gmail.com>
2019-05-20 17:37:35 +0530

    Array Elements of push, pop, shift, unshift, Concatination

---
+++ ArrayExamples.js
@@ -0,0 +1,20 @@
+var arr = [2, 3, 4];
+arr.push(5);
+console.log(arr);
+
+var arr1 = [1, 2, 3];
+arr1.unshift(0);
+console.log(arr1);
+
+var arr3 = [1, 2, 3];
+arr3.pop();
+console.log(arr3);
+
+var arr4 = [3, 4, 5];
+arr4.shift();
+console.log(arr4);
+
+var Con = [1, 2, 3];
+var Con1 = [4, 5, 6];
+var Con2 = Con.concat(Con1);
+console.log(Con2);
