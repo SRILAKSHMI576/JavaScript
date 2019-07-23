37a2ce9ae5531afb2a0654455e8a3b21fc951b24
srilakshmi576 <srilakshmigulla576@gmail.com>
2019-07-23 16:45:41 +0530

    Practice Forloop problems

---
+++ ForLoopEx.js
@@ -0,0 +1,31 @@
+// for (let i = 0; i < 0; i++) {
+//   bounceBall();
+//   const timeBounced = i + 1;
+//   console.log("time is " + timeBounced + " times");
+// }
+// console.log("fail");
+
+// const fruitBasket = ["banana", "pear", "guava"];
+
+// // fruitBasket.length is 3
+// for (let i = 0; i < fruitBasket.length; i++) {
+//   console.log("There's a " + fruitBasket[i] + " in the basket");
+// }
+
+// for (let i = fruitBasket.length - 1; i >= 0; i--) {
+//   console.log("There's a " + fruitBasket[i] + " in the basket");
+// }
+
+// const fruitBasket = ["banana", "pear", "guava"];
+// for (let fruit of fruitBasket) {
+//   console.log(fruit);
+// }
+
+// const numbers = [25, 22, 12, 56, 8, 18, 34];
+// let smallerThan20 = [];
+
+// for (let num of numbers) {
+//   if (num < 20) {
+//     smallerThan20.push(num);
+//   }
+// }
