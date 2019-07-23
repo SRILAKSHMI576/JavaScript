0dbe3992932bf5c8dc33cd99b78cb790174cc364
srilakshmi576 <srilakshmigulla576@gmail.com>
2019-07-23 17:15:59 +0530

    adds a string ending to each member in an array

---
+++ WordEndings.js
@@ -0,0 +1,7 @@
+function addEnding(arr, ending) {
+  result = arr.map(end => end + ending);
+  console.log(result);
+}
+addEnding(["clever", "meek", "hurried", "nice"], "ly");
+addEnding(["new", "pander", "scoop"], "er");
+addEnding(["bend", "sharpen", "mean"], "ing");
