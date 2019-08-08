2ce4f5352568b86628ddaca8f988e1f26c1635dd
srilakshmi576 <srilakshmigulla576@gmail.com>
2019-08-09 02:29:09 +0530

    Practice Srcimba.com examples

---
+++ ScrimbaJS.js
@@ -0,0 +1,13 @@
+// var example = true;
+// console.log(example);
+// console.log(typeof example);
+
+let firstName = "Sri";
+let lastName = "laxmi";
+console.log(firstName + " " + lastName);
+console.log(`${firstName} ${lastName}`);
+console.log(`${firstName} ${lastName}`.length);
+console.log(`${firstName} ${lastName}`.toUpperCase());
+console.log(`${firstName} ${lastName}`.toLowerCase());
+console.log(`${firstName} ${lastName}`.split(" "));
+console.log(`${firstName} ${lastName}`.split(""));
