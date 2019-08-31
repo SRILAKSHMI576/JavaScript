e3254a8d4ac50c7d8f9e32d8d3af280f2fb85738
srilakshmi576 <srilakshmigulla576@gmail.com>
2019-08-31 15:30:11 +0530

    Practice Json example

--- jsonEx.js
+++ jsonEx.js
@@ -6,3 +6,10 @@ console.log(JSON.stringify(objectLiteral));
 
 var jsonValue = JSON.parse('{"firstname": "Mary", "lastname": true}');
 console.log(jsonValue);
+
+function log(a) {
+  a();
+}
+log(function() {
+  console.log("hi");
+});
