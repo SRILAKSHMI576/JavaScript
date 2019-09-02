8348ede491d48ab12950d1b7f35b57c307b2aad0
srilakshmi576 <srilakshmigulla576@gmail.com>
2019-09-02 18:37:10 +0530

    Practice argument

---
+++ argumentsSpread.js
@@ -0,0 +1,11 @@
+function greet(firstname, lastname, language) {
+  console.log(firstname);
+  console.log(lastname);
+  console.log(language);
+  console.log("--------------");
+}
+
+greet();
+greet("Srilakshmi");
+greet("john", "doe");
+greet("john", "doe", "es");
