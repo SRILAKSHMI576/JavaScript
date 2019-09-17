6eb48f81e4d2e94c9acfd9d8486d419dbd1ea892
srilakshmi576 <srilakshmigulla576@gmail.com>
2019-09-17 12:54:54 +0530

    Print numbers in reverse order

--- 1.js
+++ 1.js
@@ -2,6 +2,10 @@
 //   console.log(counter);
 // }
 
-for (let counter = 5; counter <= 10; counter++) {
+// for (let counter = 5; counter <= 10; counter++) {
+//   console.log(counter);
+// }
+
+for (let counter = 3; counter >= 0; counter--) {
   console.log(counter);
 }
