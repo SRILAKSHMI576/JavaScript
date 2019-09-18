5f9f1d3e384786698d6684c35ee7e3fa572b6e96
srilakshmi576 <srilakshmigulla576@gmail.com>
2019-09-18 21:35:04 +0530

    Print sum and average of 10 numbers

--- SumOfNaturalNumbers.js
+++ SumOfNaturalNumbers.js
@@ -6,12 +6,23 @@
 // }
 // console.log("The Sum is: " + sum);
 
-var testCase = prompt();
+// var testCase = prompt();
+// sum = 0;
+// console.log("The first 7 natural number is: ");
+// for (let number = 1; number <= 7; number++) {
+//   console.log(number);
+//   sum += number;
+// }
+
+// console.log("The Sum of Natural Number upto 7 terms: " + sum);
+
 sum = 0;
-console.log("The first 7 natural number is: ");
-for (let number = 1; number <= 7; number++) {
+avg = 0;
+console.log("The first 10 natural number is: ");
+for (let number = 1; number <= 10; number++) {
   console.log(number);
   sum += number;
+  avg = sum / 10;
 }
-
-console.log("The Sum of Natural Number upto 7 terms: " + sum);
+console.log("The Sum is: " + sum);
+console.log("The Average is: " + avg);
