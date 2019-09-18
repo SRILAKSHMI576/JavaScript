0c6a0d391f93e154cae29b8c3373006ee7f2207f
srilakshmi576 <srilakshmigulla576@gmail.com>
2019-09-18 16:19:54 +0530

    Print sum of 10 natural numbers

--- SumOfNaturalNumbers.js
+++ SumOfNaturalNumbers.js
@@ -1,5 +1,7 @@
 sum = 0;
+console.log("The first 10 natural number is: ");
 for (let number = 1; number <= 10; number++) {
+  console.log(number);
   sum += number;
 }
-console.log(sum);
+console.log("The Sum is: " + sum);
