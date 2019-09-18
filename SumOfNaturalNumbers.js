6f74c3a7bd56effb02839d9a8a5a50cd8ab72a44
srilakshmi576 <srilakshmigulla576@gmail.com>
2019-09-18 18:32:50 +0530

    Print sum of 7  natural numbers

--- SumOfNaturalNumbers.js
+++ SumOfNaturalNumbers.js
@@ -1,7 +1,17 @@
+// sum = 0;
+// console.log("The first 10 natural number is: ");
+// for (let number = 1; number <= 10; number++) {
+//   console.log(number);
+//   sum += number;
+// }
+// console.log("The Sum is: " + sum);
+
+var testCase = prompt();
 sum = 0;
-console.log("The first 10 natural number is: ");
-for (let number = 1; number <= 10; number++) {
+console.log("The first 7 natural number is: ");
+for (let number = 1; number <= 7; number++) {
   console.log(number);
   sum += number;
 }
-console.log("The Sum is: " + sum);
+
+console.log("The Sum of Natural Number upto 7 terms: " + sum);
