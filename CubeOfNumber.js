3a65050b887cd44bd94a27facfc5cbadc665e81f
srilakshmi576 <srilakshmigulla576@gmail.com>
2019-09-19 11:14:46 +0530

    print a Cube Of 5 numbers in a function

--- CubeOfNumber.js
+++ CubeOfNumber.js
@@ -1,4 +1,7 @@
-for (let number = 1; number <= 5; number++) {
-  cube = number * number * number;
-  console.log(number + " and cube of the " + number + " is: " + cube);
+function cubeOfNumber() {
+  for (let number = 1; number <= 5; number++) {
+    cube = number * number * number;
+    console.log(number + " and cube of the " + number + " is: " + cube);
+  }
 }
+cubeOfNumber();
