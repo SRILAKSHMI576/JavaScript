e2d9515b57844712069bf4d94c9e60594b72a6a6
srilakshmi576 <srilakshmigulla576@gmail.com>
2019-09-19 15:28:42 +0530

    Print 10 odd numbers and Sum

--- CubeOfNumber.js
+++ CubeOfNumber.js
@@ -1,23 +1,35 @@
-function cubeOfNumber() {
-  for (let number = 1; number <= 5; number++) {
-    cube = number * number * number;
-    console.log(number + " and cube of the " + number + " is: " + cube);
-  }
-}
-cubeOfNumber();
+// function cubeOfNumber() {
+//   for (let number = 1; number <= 5; number++) {
+//     cube = number * number * number;
+//     console.log(number + " and cube of the " + number + " is: " + cube);
+//   }
+// }
+// cubeOfNumber();
 
-function MultiplicationTable() {
-  for (let number = 1; number <= 10; number++) {
-    console.log("15 * " + number + " = " + 15 * number);
-  }
-}
-MultiplicationTable();
+// function MultiplicationTable() {
+//   for (let number = 1; number <= 10; number++) {
+//     console.log("15 * " + number + " = " + 15 * number);
+//   }
+// }
+// MultiplicationTable();
 
-function MultiplicationTables() {
-  for (let number = 1; number <= 8; number++) {
-    for (let num = 1; num <= 10; num++) {
-      console.log(number + " * " + num + " = " + number * num);
+// function MultiplicationTables() {
+//   for (let number = 1; number <= 8; number++) {
+//     for (let num = 1; num <= 10; num++) {
+//       console.log(number + " * " + num + " = " + number * num);
+//     }
+//   }
+// }
+// MultiplicationTables();
+
+function oddNumbersAndSum() {
+  sum = 0;
+  for (let i = 1; i <= 20; i++) {
+    if (i % 2 != 0) {
+      console.log(i);
+      sum += i;
     }
   }
+  console.log("The sum is: " + sum);
 }
-MultiplicationTables();
+oddNumbersAndSum();
