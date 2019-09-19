0cd0b4912fcca172db583390dda14707fc9f353a
srilakshmi576 <srilakshmigulla576@gmail.com>
2019-09-19 15:06:03 +0530

    Print 1 to 8 multiplication tables

--- CubeOfNumber.js
+++ CubeOfNumber.js
@@ -7,8 +7,17 @@ function cubeOfNumber() {
 cubeOfNumber();
 
 function MultiplicationTable() {
-  for (let i = 1; i <= 10; i++) {
-    console.log("15 * " + i + " = " + 15 * i);
+  for (let number = 1; number <= 10; number++) {
+    console.log("15 * " + number + " = " + 15 * number);
   }
 }
 MultiplicationTable();
+
+function MultiplicationTables() {
+  for (let number = 1; number <= 8; number++) {
+    for (let num = 1; num <= 10; num++) {
+      console.log(number + " * " + num + " = " + number * num);
+    }
+  }
+}
+MultiplicationTables();
