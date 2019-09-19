f034178fe73719686d13e0776c726ff1e336e398
srilakshmi576 <srilakshmigulla576@gmail.com>
2019-09-19 18:03:58 +0530

    print triangle pyramid for 4 lines

--- CubeOfNumber.js
+++ CubeOfNumber.js
@@ -1,26 +1,26 @@
-// function cubeOfNumber() {
-//   for (let number = 1; number <= 5; number++) {
-//     cube = number * number * number;
-//     console.log(number + " and cube of the " + number + " is: " + cube);
-//   }
-// }
-// cubeOfNumber();
+function cubeOfNumber() {
+  for (let number = 1; number <= 5; number++) {
+    cube = number * number * number;
+    console.log(number + " and cube of the " + number + " is: " + cube);
+  }
+}
+cubeOfNumber();
 
-// function MultiplicationTable() {
-//   for (let number = 1; number <= 10; number++) {
-//     console.log("15 * " + number + " = " + 15 * number);
-//   }
-// }
-// MultiplicationTable();
+function MultiplicationTable() {
+  for (let number = 1; number <= 10; number++) {
+    console.log("15 * " + number + " = " + 15 * number);
+  }
+}
+MultiplicationTable();
 
-// function MultiplicationTables() {
-//   for (let number = 1; number <= 8; number++) {
-//     for (let num = 1; num <= 10; num++) {
-//       console.log(number + " * " + num + " = " + number * num);
-//     }
-//   }
-// }
-// MultiplicationTables();
+function MultiplicationTables() {
+  for (let number = 1; number <= 8; number++) {
+    for (let num = 1; num <= 10; num++) {
+      console.log(number + " * " + num + " = " + number * num);
+    }
+  }
+}
+MultiplicationTables();
 
 function oddNumbersAndSum() {
   sum = 0;
@@ -33,3 +33,13 @@ function oddNumbersAndSum() {
   console.log("The sum is: " + sum);
 }
 oddNumbersAndSum();
+
+function trianglePyramid() {
+  for (let i = 1; i <= 4; i++) {
+    for (let j = 1; j <= i; j++) {
+      console.log("*");
+    }
+    console.log();
+  }
+}
+trianglePyramid();
