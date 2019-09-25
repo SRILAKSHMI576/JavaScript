6b2d1f51076d86bc76c13f70df79f695bf86f5b0
srilakshmi576 <srilakshmigulla576@gmail.com>
2019-09-25 11:05:31 +0530

    Replace arr1[0] element with new element

--- Arrays.js
+++ Arrays.js
@@ -1,2 +1,7 @@
-arr1 = ["a", "e", "i", "o", "u"];
-console.log(arr1[0]);
+function BasicArray() {
+  arr1 = ["a", "e", "i", "o", "u"];
+  console.log(arr1[0]);
+  arr1[0] = "s";
+  console.log(arr1);
+}
+BasicArray();
