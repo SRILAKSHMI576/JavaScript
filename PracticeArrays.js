c58a1e0145e700b12761a562ceb24ea216d0c6b8
srilakshmi576 <srilakshmigulla576@gmail.com>
2019-09-25 11:48:21 +0530

    Print reverse of an array

--- PracticeArrays.js
+++ PracticeArrays.js
@@ -1,8 +1,16 @@
-// Return the first element in an array
-
+// 1...Return the first element in an array
 function getFirstValue(arr) {
   console.log(arr[0]);
 }
-getFirstValue([1, 2, 3]);
-getFirstValue([80, 5, 100]);
-getFirstValue([-500, 0, 50]);
+// getFirstValue([1, 2, 3]);
+// getFirstValue([80, 5, 100]);
+// getFirstValue([-500, 0, 50]);
+
+//2...Reverse an Array
+
+function reverseArray(arr) {
+  console.log(arr.reverse());
+}
+reverseArray([1, 2, 3, 4]);
+reverseArray([9, 9, 2, 3, 4]);
+reverseArray([]);
