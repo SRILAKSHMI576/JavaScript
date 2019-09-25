c27ff45d9a3507c87749652b76ba7f604a55591e
srilakshmi576 <srilakshmigulla576@gmail.com>
2019-09-25 11:41:33 +0530

    Print first element in an array

---
+++ PracticeArrays.js
@@ -0,0 +1,8 @@
+// Return the first element in an array
+
+function getFirstValue(arr) {
+  console.log(arr[0]);
+}
+getFirstValue([1, 2, 3]);
+getFirstValue([80, 5, 100]);
+getFirstValue([-500, 0, 50]);
