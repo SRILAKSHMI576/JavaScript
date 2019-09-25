54a57c4a0d3cec0bcafee2497c09e5c09fe9d5ee
srilakshmi576 <srilakshmigulla576@gmail.com>
2019-09-25 11:24:30 +0530

    Push and pop a element in an array

--- Arrays.js
+++ Arrays.js
@@ -6,5 +6,10 @@ function BasicArray() {
   console.log(arr1.length);
   console.log(arr1.sort()); // Alphabet order
   console.log(arr1[arr1.length - 1]);
+  arr2 = arr1.push("m");
+  console.log(arr2);
+  console.log(arr1);
+  arr2 = arr1.pop();
+  console.log(arr1);
 }
 BasicArray();
