193bf4256487c4306537d75c0020a46b3069bf4f
srilakshmi576 <srilakshmigulla576@gmail.com>
2019-09-30 16:43:45 +0530

    Print students names

--- PracticeArrays.js
+++ PracticeArrays.js
@@ -235,7 +235,14 @@ function checkEquals(arr1, arr2) {
 function filterDigitLength(arr, num) {
   console.log(arr.filter(numbers => numbers.toString().length == num));
 }
-filterDigitLength([88, 232, 4, 9721, 555], 3);
-filterDigitLength([2, 7, 8, 9, 1012], 1);
-filterDigitLength([32, 88, 74, 91, 300, 4050], 1);
-filterDigitLength([5, 6, 8, 9], 1);
+// filterDigitLength([88, 232, 4, 9721, 555], 3);
+// filterDigitLength([2, 7, 8, 9, 1012], 1);
+// filterDigitLength([32, 88, 74, 91, 300, 4050], 1);
+// filterDigitLength([5, 6, 8, 9], 1);
+
+//27...Get Student Names
+
+function getStudentsNames(students) {
+  console.log(students.map(a => a.name));
+}
+getStudentsNames([{ name: "Steve" }, { name: "Mike" }, { name: "John" }]);
