ced9f7bddeeba68e4deaf45cf2f17a0b2016a546
srilakshmi576 <srilakshmigulla576@gmail.com>
2019-10-07 12:34:37 +0530

    Written an array which are divisible by three and five

--- Arrays.js
+++ Arrays.js
@@ -16,4 +16,15 @@ function arrayDivisibleByThree() {
     }
   }
 }
-arrayDivisibleByThree();
+// arrayDivisibleByThree();
+
+function DivisibleByThreeFive() {
+  var arr = [1, 3, 4, 7, 8, 9, 10, 11, 12, 14, 15, 21, 27];
+  n = arr.length - 1;
+  for (let i = 0; i <= n; i++) {
+    if (arr[i] % 3 == 0 && arr[i] % 5 == 0) {
+      console.log(arr[i]);
+    }
+  }
+}
+DivisibleByThreeFive();
