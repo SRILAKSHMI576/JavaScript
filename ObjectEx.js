9c47c91918888e295417d8ee4528d27b5b24f8c0
SRILAKSHMI576 <srilakshmigulla576@gmail.com>
2019-10-11 11:47:47 +0530

    Write a basic object program

--- ObjectEx.js
+++ ObjectEx.js
@@ -19,3 +19,29 @@ person.address.state = "Ny";
 console.log(person.address.street);
 console.log(person.address.city);
 console.log(person["address"]["state"]);
+
+var Tony = {
+    firstname: "Tony",
+    lastname: "Alicea",
+    address: {
+      street: "111 Main St.",
+      city: "New York",
+      state: "NY"
+    }
+  };
+  
+  function greet(person) {
+    console.log("Hi " + person.firstname);
+  }
+  
+  greet(Tony);
+  
+  greet({
+    firstname: "Mary",
+    lastname: "Doe"
+  });
+  
+  Tony.address2 = {
+    street: "333 Second St."
+  };
+  
